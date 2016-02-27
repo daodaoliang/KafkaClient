878ae494b6dfd336283e279cb02b329ab3db91d6
andrew <Andrew.Kostousov@gmail.com>
2016-02-27 22:17:35 +0500

    use confluent vm ip address

--- KafkaClient/KafkaClient/Config.cs
+++ KafkaClient/KafkaClient/Config.cs
@@ -2,7 +2,7 @@
 {
     public class Config
     {
-        public static string HostName = "192.168.190.129:8082";
+        public static string HostName = "192.168.33.10:8082";
         public static string TopicName = "JsonTestTopic";
     }
 }
