506d9d8968f8f5c6228e83ca909481950d308d37
fakefeik <feikfake1@gmail.com>
2016-03-01 22:46:00 +0500

    start confluent on vagrant up

---
+++ confluent/start-confluent.sh
@@ -0,0 +1,8 @@
+# Start Zookeeper
+(/usr/bin/zookeeper-server-start /etc/kafka/zookeeper.properties) &
+
+# Start Kafka
+(sleep 2; /usr/bin/kafka-server-start /etc/kafka/server.properties) &
+
+# Start REST server
+(sleep 4; /usr/bin/kafka-rest-start /etc/kafka-rest/kafka-rest.properties)
