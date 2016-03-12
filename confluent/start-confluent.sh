ffd97e192a8a8c3951c9e3df3bb7119654b5ab3e
fakefeik <feikfake1@gmail.com>
2016-03-12 15:51:54 +0500

    confluent basic logging

--- confluent/start-confluent.sh
+++ confluent/start-confluent.sh
@@ -1,10 +1,16 @@
+cd /vagrant
+
+if [ ! -d "kafka-logs" ]; then
+	mkdir kafka-logs
+fi
+
 # Start Zookeeper
-(/usr/bin/zookeeper-server-start /etc/kafka/zookeeper.properties) &
+(/usr/bin/zookeeper-server-start /etc/kafka/zookeeper.properties > /vagrant/kafka-logs/zookeper-log.txt) &
 
 # Start Kafka
-(sleep 2; /usr/bin/kafka-server-start /etc/kafka/server.properties) &
+(sleep 2; /usr/bin/kafka-server-start /etc/kafka/server.properties > /vagrant/kafka-logs/server-log.txt) &
 
 # Start REST server
-(sleep 4; /usr/bin/kafka-rest-start /etc/kafka-rest/kafka-rest.properties) &
+(sleep 4; /usr/bin/kafka-rest-start /etc/kafka-rest/kafka-rest.properties > /vagrant/kafka-logs/kafka-rest-log.txt) &
 
 (echo confluent started)
