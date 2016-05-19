8f232e9370af862d25bf2a5786fe9648b0497389
fakefeik <feikfake1@gmail.com>
2016-05-19 19:03:23 +0500

    add IkvmKafkaClient

---
+++ IkvmKafkaClient/Consumer/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using java.util;
+using org.apache.kafka.clients.consumer;
+using org.apache.kafka.common.serialization;
+
+namespace Consumer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = "192.168.33.12:9092";
+            var topic = "test2";
+            var count = 50000000;
+
+            var prop = new Properties();
+            prop.setProperty("bootstrap.servers", host);
+            prop.setProperty("group.id", "testgroup");
+            prop.setProperty("auto.offset.reset", "earliest");
+            //prop.setProperty("enable.auto.commit", "true");
+            //prop.setProperty("auto.commit.interval.ms", "1000");
+            var c = new KafkaConsumer(prop, new ByteArrayDeserializer(), new ByteArrayDeserializer());
+
+            var topics = new ArrayList(1);
+            topics.add(topic);
+            var time = DateTime.UtcNow;
+            c.subscribe(topics);
+            var bytes = 0;
+            var i = count;
+            while (i > 0)
+            {
+                var r = c.poll(1000);
+                var records = r.records(topic);
+                while (records.iterator().hasNext() && i > 0)
+                {
+                    var rec = records.iterator().next();
+                    var s = (ConsumerRecord)rec;
+                    var b = (byte[])s.value();
+                    bytes += b.Length;
+                    i--;
+                }
+            }
+            var mb = bytes / 1024.0 / 1024.0;
+            var seconds = (DateTime.UtcNow - time).TotalSeconds;
+            Console.WriteLine($"{mb / seconds} MB/sec");
+            Console.WriteLine($"{count / seconds} Msg/sec");
+        }
+    }
+}
---
+++ IkvmKafkaClient/Producer/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using java.util;
+using org.apache.kafka.clients.producer;
+using org.apache.kafka.common.serialization;
+using Exception = java.lang.Exception;
+
+namespace Producer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = "192.168.33.12:9092";
+            var topic = "test2";
+            var count = 50000000;
+            var size = 100;
+
+            var prop = new Properties();
+            prop.setProperty("bootstrap.servers", "192.168.33.12:9092");
+            prop.setProperty("acks", "1");
+            prop.setProperty("buffer.memory", "67108864");
+            prop.setProperty("batch.size", "8196");
+            
+            var producer = new KafkaProducer(prop, new ByteArraySerializer(), new ByteArraySerializer());
+            var payload = new byte[size];
+            for (int i = 0; i < size; i++)
+                payload[i] = (byte)'a';
+
+            var record = new ProducerRecord(topic, payload);
+            var stats = new Stats(count, 5000, Console.WriteLine);
+
+            for (int i = 0; i < count; i++)
+            {
+                var sendStart = DateTimeExtensions.CurrentTimeMillis();
+                var cb = new StatsCallback { Action = stats.NextCompletion(sendStart, payload.Length, stats) };
+                producer.send(record, cb);
+            }
+            producer.close();
+            stats.PrintTotal();
+        }
+    }
+
+    public class StatsCallback : Callback
+    {
+        public Action Action;
+
+        public void onCompletion(RecordMetadata rm, Exception e)
+        {
+            Action();
+        }
+    }
+}
